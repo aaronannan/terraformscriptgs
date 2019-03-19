132346f7064917e28200cbf4b2411a4b2a42bf07
aaronannan <42162864+aaronannan@users.noreply.github.com>
2019-03-19 16:37:07 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIAJR3EBLCALXQ7JDNA"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "us-east-1"
     
      
